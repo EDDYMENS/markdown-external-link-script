55da8f2393b8ab91d48b2300dd9c0933f3d7c166
Edmond Mensah <primerossgh@gmail.com>
2023-05-09 21:06:26 +0200

    replace btn with link var naming

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var links = document.body.getElementsByTagName("a");
-var btnCount = links.length;
-for (var i = 0; i < btnCount; i++) {
+var linkCount = links.length;
+for (var i = 0; i < linkCount; i++) {
     if (links[i].textContent.includes('‌')) { // It might seem empty but it checking for a Zero Width Non-Joiner character
         links[i].setAttribute('target', '_blank');
     }
