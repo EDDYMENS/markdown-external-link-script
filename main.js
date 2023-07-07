b708f6a5c5a65fad41a740a15d7e36c0e0fdc65e
Edmond Mensah <primerossgh@gmail.com>
2023-07-07 20:39:11 +0200

    drop support for {_ext} keyword

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-(function(){
-var links = document.body.getElementsByTagName("a");
-var linkCount = links.length;
-const keyword = ' {_ext}';
-for (var i = 0; i < linkCount; i++) {
-    if (links[i].textContent.includes(keyword)) {
-        links[i].setAttribute('target', '_blank');
-        links[i].textContent = links[i].textContent.replace(keyword, '');
+(function() {
+    var links = document.body.getElementsByTagName("a");
+    var linkCount = links.length;
+    for (var i = 0; i < linkCount; i++) {
+        if (new URL(links[i].href).origin !== location.origin) {
+            links[i].setAttribute('target', '_blank');
+        }
     }
-}
 })();
